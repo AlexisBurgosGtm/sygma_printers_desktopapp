e7f7c00ff6a1da41d518a2555b13521880e4ee1c
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2025-04-28 16:38:32 -0600

    inicial

---
+++ XELASOL_PRINTERS/inicio.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class inicio
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(inicio))
+        Me.GroupControl1 = New DevExpress.XtraEditors.GroupControl()
+        Me.gridEmbarques = New DevExpress.XtraGrid.GridControl()
+        Me.GridViewEmbarques = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GroupControl2 = New DevExpress.XtraEditors.GroupControl()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.lbEmbarque = New DevExpress.XtraEditors.LabelControl()
+        Me.GroupControl3 = New DevExpress.XtraEditors.GroupControl()
+        Me.cmbMes = New System.Windows.Forms.ComboBox()
+        Me.cmbAnio = New System.Windows.Forms.ComboBox()
+        Me.cmbActivo = New System.Windows.Forms.ComboBox()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupControl1.SuspendLayout
+        CType(Me.gridEmbarques, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GridViewEmbarques, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GroupControl2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GroupControl3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupControl3.SuspendLayout
+        Me.SuspendLayout()
+        '
+        'GroupControl1
+        '
+        Me.GroupControl1.Controls.Add(Me.gridEmbarques)
+        Me.GroupControl1.Location = New System.Drawing.Point(12, 95)
+        Me.GroupControl1.Name = "GroupControl1"
+        Me.GroupControl1.Size = New System.Drawing.Size(858, 491)
+        Me.GroupControl1.TabIndex = 0
+        Me.GroupControl1.Text = "LISTA GENERAL DE EMBARQUES"
+        '
+        'gridEmbarques
+        '
+        Me.gridEmbarques.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.gridEmbarques.Location = New System.Drawing.Point(2, 20)
+        Me.gridEmbarques.MainView = Me.GridViewEmbarques
+        Me.gridEmbarques.Name = "gridEmbarques"
+        Me.gridEmbarques.Size = New System.Drawing.Size(854, 469)
+        Me.gridEmbarques.TabIndex = 0
+        Me.gridEmbarques.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridViewEmbarques})
+        '
+        'GridViewEmbarques
+        '
+        Me.GridViewEmbarques.Appearance.Row.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.GridViewEmbarques.Appearance.Row.Options.UseBackColor = True
+        Me.GridViewEmbarques.GridControl = Me.gridEmbarques
+        Me.GridViewEmbarques.Name = "GridViewEmbarques"
+        Me.GridViewEmbarques.OptionsBehavior.Editable = False
+        Me.GridViewEmbarques.OptionsView.ShowAutoFilterRow = True
+        '
+        'GroupControl2
+        '
+        Me.GroupControl2.Location = New System.Drawing.Point(885, 95)
+        Me.GroupControl2.Name = "GroupControl2"
+        Me.GroupControl2.Size = New System.Drawing.Size(236, 491)
+        Me.GroupControl2.TabIndex = 1
+        Me.GroupControl2.Text = "IMPRESIONES"
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(678, 32)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(117, 13)
+        Me.LabelControl1.TabIndex = 2
+        Me.LabelControl1.Text = "Embarque Seleccionado:"
+        '
+        'lbEmbarque
+        '
+        Me.lbEmbarque.Appearance.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbEmbarque.Appearance.ForeColor = System.Drawing.Color.Red
+        Me.lbEmbarque.Appearance.Options.UseFont = True
+        Me.lbEmbarque.Appearance.Options.UseForeColor = True
+        Me.lbEmbarque.Location = New System.Drawing.Point(678, 53)
+        Me.lbEmbarque.Name = "lbEmbarque"
+        Me.lbEmbarque.Size = New System.Drawing.Size(6, 18)
+        Me.lbEmbarque.TabIndex = 3
+        Me.lbEmbarque.Text = "-"
+        '
+        'GroupControl3
+        '
+        Me.GroupControl3.Controls.Add(Me.LabelControl2)
+        Me.GroupControl3.Controls.Add(Me.cmbActivo)
+        Me.GroupControl3.Controls.Add(Me.cmbAnio)
+        Me.GroupControl3.Controls.Add(Me.cmbMes)
+        Me.GroupControl3.Location = New System.Drawing.Point(12, 9)
+        Me.GroupControl3.Name = "GroupControl3"
+        Me.GroupControl3.Size = New System.Drawing.Size(653, 80)
+        Me.GroupControl3.TabIndex = 4
+        Me.GroupControl3.Text = "Parametros"
+        '
+        'cmbMes
+        '
+        Me.cmbMes.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cmbMes.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.cmbMes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbMes.FormattingEnabled = True
+        Me.cmbMes.Location = New System.Drawing.Point(332, 51)
+        Me.cmbMes.Name = "cmbMes"
+        Me.cmbMes.Size = New System.Drawing.Size(121, 21)
+        Me.cmbMes.TabIndex = 0
+        '
+        'cmbAnio
+        '
+        Me.cmbAnio.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cmbAnio.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.cmbAnio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbAnio.FormattingEnabled = True
+        Me.cmbAnio.Items.AddRange(New Object() {"2024", "2025", "2026", "2027", "2028", "2029", "2030", "2031", "2032", "2033", "2034", "2035", "2036", "2037", "2038", "2039", "2040", "2041", "2042", "2043", "2044", "2045", "2046", "2047", "2048", "2049", "2050"})
+        Me.cmbAnio.Location = New System.Drawing.Point(459, 51)
+        Me.cmbAnio.Name = "cmbAnio"
+        Me.cmbAnio.Size = New System.Drawing.Size(85, 21)
+        Me.cmbAnio.TabIndex = 1
+        '
+        'cmbActivo
+        '
+        Me.cmbActivo.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cmbActivo.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.cmbActivo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbActivo.FormattingEnabled = True
+        Me.cmbActivo.Items.AddRange(New Object() {"ACTIVOS", "INACTIVOS"})
+        Me.cmbActivo.Location = New System.Drawing.Point(551, 51)
+        Me.cmbActivo.Name = "cmbActivo"
+        Me.cmbActivo.Size = New System.Drawing.Size(81, 21)
+        Me.cmbActivo.TabIndex = 2
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Appearance.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelControl2.Appearance.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.LabelControl2.Appearance.Options.UseFont = True
+        Me.LabelControl2.Appearance.Options.UseForeColor = True
+        Me.LabelControl2.Location = New System.Drawing.Point(7, 26)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(301, 19)
+        Me.LabelControl2.TabIndex = 3
+        Me.LabelControl2.Text = "Modulo de Impresion de Documentos"
+        '
+        'inicio
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1133, 598)
+        Me.Controls.Add(Me.GroupControl3)
+        Me.Controls.Add(Me.lbEmbarque)
+        Me.Controls.Add(Me.LabelControl1)
+        Me.Controls.Add(Me.GroupControl2)
+        Me.Controls.Add(Me.GroupControl1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "inicio"
+        Me.Text = "inicio"
+        CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupControl1.ResumeLayout(False)
+        CType(Me.gridEmbarques, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GridViewEmbarques, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GroupControl2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GroupControl3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupControl3.ResumeLayout(False)
+        Me.GroupControl3.PerformLayout
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents gridEmbarques As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GridViewEmbarques As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents lbEmbarque As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents cmbActivo As ComboBox
+    Friend WithEvents cmbAnio As ComboBox
+    Friend WithEvents cmbMes As ComboBox
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents GroupControl1 As GroupControl
+    Friend WithEvents GroupControl2 As GroupControl
+    Friend WithEvents GroupControl3 As GroupControl
+End Class
---
+++ XELASOL_PRINTERS/inicio.vb
@@ -0,0 +1,57 @@
+﻿Public Class inicio
+
+
+
+    Private Sub inicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+
+
+        Dim tblMeses As New DataTable
+        With tblMeses.Columns
+            .Add("CODMES", GetType(String))
+            .Add("DESMES", GetType(String))
+        End With
+
+
+        With tblMeses.Rows
+            .Add(New Object() {1, "ENERO"})
+            .Add(New Object() {2, "FEBRERO"})
+            .Add(New Object() {3, "MARZO"})
+            .Add(New Object() {4, "ABRIL"})
+            .Add(New Object() {5, "MAYO"})
+            .Add(New Object() {6, "JUNIO"})
+            .Add(New Object() {7, "JULIO"})
+            .Add(New Object() {8, "AGOSTO"})
+            .Add(New Object() {9, "SEPTIEMBRE"})
+            .Add(New Object() {10, "OCTUBRE"})
+            .Add(New Object() {11, "NOVIEMBRE"})
+            .Add(New Object() {12, "DICIEMBRE"})
+        End With
+
+
+        With Me.cmbMes
+            .DataSource = tblMeses
+            .ValueMember = "CODMES"
+            .DisplayMember = "DESMES"
+        End With
+
+        Me.cmbMes.SelectedValue = Today.Month
+        Me.cmbAnio.Text = Today.Year.ToString
+
+
+
+        Call CargarGrid()
+
+
+    End Sub
+
+
+    Private Sub CargarGrid()
+
+
+    End Sub
+
+
+End Class
